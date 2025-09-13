eb81dead41e99139859bb7d982b7370beada6fcc
allisonecwy2028-rgb <allisonecwy2028@student.cis.edu.hk>
2025-09-13 10:06:18 +0800

    completed task

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner (System.in);
+                System.out.println("Enter your age:");
+        int age = -1;
+        while (age < 0) {
+            try {
+                age = Integer.parseInt(scanner.nextLine());
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Please enter a valid number");
+            }
+        }
+        int dateAge = 7 + (age / 2);
+        String output = age + "-year olds should date somebody who is at least " + dateAge + " years old.";
+        System.out.println(output);
+
     }
 }
